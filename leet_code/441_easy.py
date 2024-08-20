ac2f1af2878b960e624700d22cf84f58cfec5981
Enot3811 <enot3811@gmail.com>
2024-08-20 13:06:27 +0300

    Better solution for task 441

--- leet_code/441_easy.py
+++ leet_code/441_easy.py
@@ -3,17 +3,36 @@
 https://leetcode.com/problems/arranging-coins/description/
 """
 
+# class Solution:
+#     def arrangeCoins(self, n: int) -> int:
+#         sum = 0
+#         i = 0
+#         while sum < n:
+#             i += 1
+#             sum += i
+#         if sum > n:
+#             i -= 1
+#         return i
+
+
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        sum = 0
-        i = 0
-        while sum < n:
-            i += 1
-            sum += i
-        if sum > n:
-            i -= 1
-        return i
-    
+        # k * (k + 1) / 2 <= n
+        # Необходимо найти наибольший k, удовлетворяющий этому условию
+        # Ищем бинарным поиском среди чисел 0...n
+        left = 0
+        right = n
+        while left <= right:
+            k = (left + right) // 2
+            cur_sum = k * (k + 1) / 2
+            if cur_sum < n:
+                left = k + 1
+            elif cur_sum > n:
+                right = k - 1
+            else:
+                return k
+        return right
+
 
 sol = Solution()
 samples = [(0, 0), (1, 1), (5, 2), (8, 3)]
