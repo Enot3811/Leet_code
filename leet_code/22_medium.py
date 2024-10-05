f1b99865a6a11a8585f8cc217c8ea9a0b618e0f9
Enot3811 <enot3811@gmail.com>
2024-10-05 15:04:05 +0300

    22 task leet code

---
+++ leet_code/22_medium.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        open_cnt = n
+        close_cnt = 0
+        current_str = []
+        variants = Solution.solve_step(current_str, open_cnt, close_cnt)
+        return variants
+
+    @staticmethod
+    def solve_step(
+        current_str: List[str], open_cnt: int, close_cnt: int
+    ) -> List[str]:
+        if open_cnt == 0 and close_cnt == 0:
+            return [''.join(current_str)]
+        variants = []
+        if open_cnt > 0:
+            # Add new variants when we open parentheses
+            current_str.append('(')
+            variants += Solution.solve_step(
+                current_str, open_cnt - 1, close_cnt + 1)
+            current_str.pop()
+        if close_cnt > 0:
+            # Add new variant when we close parentheses
+            current_str.append(')')
+            variants += Solution.solve_step(
+                current_str, open_cnt, close_cnt - 1)
+            current_str.pop()
+        return variants
+
+
+sol = Solution()
+
+examples = [
+    (1, ['()']),
+    (2, ['()()', '(())']),
+    (3, ['()()()', '(())()', '()(())', '((()))', '(()())']),
+    (4, ["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())",
+         "(()())()","(())(())","(())()()","()((()))","()(()())","()(())()",
+         "()()(())","()()()()"])
+]
+
+for inp, ans in examples:
+    out = sol.generateParenthesis(inp)
+    print(inp, out, ans, set(out) == set(ans))
